0d4a6265bddde4730ab612488c3b92e12cc5473c
Matthias Mueller <matthiasm@live.de>
2017-08-14 16:06:34 +0200

    Release

--- .scripts/Internals/Build_Release.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MsBuild.exe" "C:\MyGit\Personal\KnowHowBase\Server\KHB.sln" /t:Build /p:Configuration=Release /p:TargetFramework=netcoreapp1.1
-
-exit
--- .scripts/Release.bat
+++ .scripts/Release.bat
@@ -1,4 +1,4 @@
 cd .\Internals
-
+Powershell.exe -executionpolicy remotesigned -File .\Build_Release.ps1
 Powershell.exe -executionpolicy remotesigned -File .\CommitAndPush_Git.ps1
 
